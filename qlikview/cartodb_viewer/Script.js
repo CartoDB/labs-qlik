b3d68a7d59e843506d9bd126bb2e03ce20284ac2
Dani Carrion <daniel@cartodb.com>
2015-02-27 12:37:20 -0500

    changed extension name

--- qlikview/cartodb_viewer/Script.js
+++ qlikview/cartodb_viewer/Script.js
@@ -1,6 +1,6 @@
 Qva.LoadCSS('http://libs.cartocdn.com/cartodb.js/v3/3.12/themes/css/cartodb.css');
 Qva.LoadScript('http://libs.cartocdn.com/cartodb.js/v3/3.12/cartodb.js', function () {
-    Qva.AddExtension('cartodb', function() { 
+    Qva.AddExtension('cartodb_viewer', function() { 
         this.Element.innerHTML = '<div id="map" style="width: 100%; height: 100%"></div>';
         cartodb.createVis('map', this.Layout.Text0.text, {
             zoom: 3,
